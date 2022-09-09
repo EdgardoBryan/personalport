73da6f61ce9ffeb4ad1bbb5014b96315f6f9eb41
EdgardoBryan <edgardobryanescamilla@gmail.com>
2022-09-09 09:21:53 -0400

    Added the top bar and image

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import MyPic from "./MyPic.jpeg"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="TopBarHeader">
+        <h1>Edgardo Escamilla</h1>
+        <div className="moreinfotabs">
+          <div>
+            <a href="https://www.linkedin.com/in/edgardo-bryan-escamilla/">
+              Linkden
+            </a>
+          </div>
+          <div>
+            <a href="https://github.com/EdgardoBryan">Github</a>
+          </div>
+          <div>Contact</div>
+        </div>
+      </div>
+      <div className="ProfileDescription">
+        <div className="leftsidebio">
+          <h3>
+            Hello My Name is Edgardo Escamilla and I am a highly motivated
+            fullstack developer who specializes in javascript using react js
+          </h3>
+        </div>
+        <div className="RightSideImage">
+          <img src={MyPic}  alt="edgardo"/>
+        </div>
+      </div>
     </div>
   );
 }
