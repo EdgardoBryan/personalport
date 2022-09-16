70f8ddf0d35f122415865ffd658ec49285871765
EdgardoBryan <edgardobryanescamilla@gmail.com>
2022-09-16 10:05:16 -0400

    v1finished

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -27,7 +27,7 @@ function App() {
               Github
             </a>
           </div>
-          <div>Contact</div>
+          <div>FullStack Developer</div>
         </div>
       </div>
       <div className="ProfileDescription">
@@ -66,6 +66,9 @@ function App() {
         <div className="projectboxes pro1">
           <img src={Burger} alt="burgerpic" className="Menu"></img>
           <h3>BURGER MENU PAGE BUILT USING HTML AND CSS</h3>
+          <a href="https://codepen.io/edgardocodes11/pen/zYRgroK">
+            <button className="button1">Code Base</button>
+          </a>
         </div>
         <div className="projectboxes pro2">
           <img
@@ -74,10 +77,16 @@ function App() {
             className="WebsitesBuilder"
           ></img>
           <h3>FRONT END DESIGN BUILT USING REACT JS HTML AND CSS</h3>
+          <a href="https://codesandbox.io/s/do-it-right-page-lhkytg">
+            <button className="button2">Code Base</button>
+          </a>
         </div>
         <div className="projectboxes pro3">
           <img src={Tupac} alt="idCard" className="tupac"></img>
           <h3>MOCK PORTFOLLIO CARD BUILT USING HTML CSS AND FONTAWESOME</h3>
+          <a href="https://codepen.io/edgardocodes11/pen/xxWOBKo">
+            <button className="button3">Code Base</button>
+          </a>
         </div>
         <div className="projectboxes pro4">
           <img
@@ -89,10 +98,16 @@ function App() {
             React MultiStep Page built using state to dynamically change pages
             using React Javascript Css and Html{" "}
           </h3>
+          <a href="https://codesandbox.io/s/react-multi-state-page-02rrpp">
+            <button className="button4">Code Base</button>
+          </a>
         </div>
         <div className="projectboxes pro5">
           <img src={instagram} alt="instagram" className="instagram"></img>
           <h3>Instagram clone page built with Html and Css</h3>
+          <a href="https://codepen.io/edgardocodes11/pen/JjLKNmQ">
+            <button className="button5">Code Base</button>
+          </a>
         </div>
         <div className="projectboxes pro6">
           <img src={holiday} alt="holiday" className="holiday"></img>
@@ -101,7 +116,20 @@ function App() {
             delete edit and create new holidays and with a database to persist
             data
           </h3>
+          <a href="https://every-day-is-a-holiday.herokuapp.com/holidays">
+            <button className="button6">Code Base</button>
+          </a>
         </div>
+      </div>
+      <h2 className="about">About ME</h2>
+      <div className="AboutMe">
+      <h3>
+        Hello Everyone! I am looking to start a new career in the tech industry.
+        I have been in the retail, child education and the fitness industry for
+        the past 7 years. My previous job experiences have given me the soft
+        skills, organizational abilities, and creativity that lead me to my new
+        career path.I am looking forward to developing my skills as a software developer.
+      </h3>
       </div>
     </div>
   );
